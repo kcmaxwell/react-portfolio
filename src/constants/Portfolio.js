e0a3488a80f1366662fa28cee20464b6a0ae2763
Kristopher Maxwell <zerotrig@gmail.com>
2022-05-13 14:00:36 -0600

    Initial commit

---
+++ src/constants/Portfolio.js
@@ -0,0 +1,13 @@
+const about = {
+  name: "Kristopher Maxwell",
+  description: "Description of me"
+};
+
+const projects = [
+  {
+    name: "Project 1",
+    description: "Project 1 description"
+  }
+];
+
+export { about, projects };
